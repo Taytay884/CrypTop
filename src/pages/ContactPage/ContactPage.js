3f8c3dcc62e0f6472ee5583a8dd02f0f3561b6f6
Taytay884 <Taytay884@yahoo.com>
2018-06-05 13:04:29 +0300

    Fixed filter bug

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,52 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import ContactList from '../../components/ContactList/ContactList'
-import Filter from '../../components/Filter/Filter'
-import './ContactPage.css';
+import ContactList from "../../components/ContactList/ContactList";
+import Filter from "../../components/Filter/Filter";
+import "./ContactPage.css";
 
 // Store
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { loadContacts } from '../../store/actions';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { loadContacts } from "../../store/actions";
 
 class ContactPage extends Component {
+  componentDidMount() {
+    this.renderContacts();
+  }
 
-    componentDidMount() {
-        this.renderContacts();
-    }
+  renderContacts = (filterBy = null) => {
+    console.log(filterBy);
+    this.props.loadContacts(filterBy);
+  };
 
-    renderContacts = (filterBy = null) => {
-        this.props.loadContacts(filterBy);
-    }
+  handleFilter = e => {
+    const input = e.target.value;
+    console.log(input);
+    let filterBy = { term: input };
+    this.renderContacts(filterBy);
+  };
 
-    handleFilter = (e) => {
-        const input = e.target.value;
-        let filterBy = { term: input };
-        this.renderContacts(filterBy);
-    }
-
-    render() {
-        return (
-            <section className="ContactPage page">
-                <h1 className="title-tab">Contacts</h1>
-                <Filter handleFilter={this.handleFilter} />
-                <ContactList contacts={this.props.contacts} />
-                <Link className="add-contact" to='/contact/new'><i className="add-contact-icon"></i></Link>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="ContactPage page">
+        <h1 className="title-tab">Contacts</h1>
+        <Filter handleFilter={this.handleFilter} />
+        <ContactList contacts={this.props.contacts} />
+        <Link className="add-contact" to="/contact/new">
+          <i className="add-contact-icon" />
+        </Link>
+      </section>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        contacts: state.contacts.contacts,
-    };
+const mapStateToProps = state => {
+  return {
+    contacts: state.contacts.contacts
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-        loadContacts,
-    }, dispatch)
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      loadContacts
+    },
+    dispatch
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ContactPage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ContactPage);
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,40 +1,42 @@
-import * as types from './types';
-import ContactService from '../services/ContactService';
-import UserService from '../services/UserService';
+import * as types from "./types";
+import ContactService from "../services/ContactService";
+import UserService from "../services/UserService";
 
 export function loadContacts(filterBy = null) {
-    return dispatch => {
-        ContactService.getContacts(filterBy).then(contacts => {
-            dispatch({ type: types.LOAD_CONTACTS, payload: contacts });
-        })
-            .catch(err => console.log('Error:', err));
-    }
+  return dispatch => {
+    ContactService.getContacts(filterBy)
+      .then(contacts => {
+        console.log(contacts);
+        dispatch({ type: types.LOAD_CONTACTS, payload: contacts });
+      })
+      .catch(err => console.log("Error:", err));
+  };
 }
 
 export function saveContact(contact = {}, callback) {
-    return dispatch => {
-        ContactService.saveContact(contact).then(contacts => {
-            dispatch({ type: types.SAVE_CONTACT, payload: contacts })
-            callback(contacts);
-        })
-    }
+  return dispatch => {
+    ContactService.saveContact(contact).then(contacts => {
+      dispatch({ type: types.SAVE_CONTACT, payload: contacts });
+      callback(contacts);
+    });
+  };
 }
 
 export function loadContact(id, callback) {
-    return dispatch => {
-        ContactService.getContactById(id).then(contact => {
-            dispatch({ type: types.LOAD_CONTACT, payload: contact })
-            callback(contact);
-        })
-    }
+  return dispatch => {
+    ContactService.getContactById(id).then(contact => {
+      dispatch({ type: types.LOAD_CONTACT, payload: contact });
+      callback(contact);
+    });
+  };
 }
 
 export function signUp(user) {
-    UserService.saveUser(user);
-    return { type: types.SIGN_UP, payload: user };
+  UserService.saveUser(user);
+  return { type: types.SIGN_UP, payload: user };
 }
 
 export function loadUser(callback) {
-    const user = UserService.getUser();
-    return { type: types.LOAD_USER, payload: user };
+  const user = UserService.getUser();
+  return { type: types.LOAD_USER, payload: user };
 }
